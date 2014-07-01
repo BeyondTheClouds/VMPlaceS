ea511caabec4c720d18530da726adaaf143a90f1
Adrien Lebre <alebre@dhcp-3-69.rech172-28.emn.fr>
2014-07-01 18:51:33 +0200

    More and more refactoring (instanciateVMs is not compiling) - Adrien

--- src/main/java/simulation/SimulatorManager.java
+++ src/main/java/simulation/SimulatorManager.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright 2012-2013-2014. The SimGrid Team. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package.
+ *
+ * This class aims at controlling the interactions between the different components of the injector simulator.
+ * It is mainly composed of static methods. Although it is rather ugly, this is the direct way to make a kind of
+ * singleton ;)
+ *
+ * @author adrien.lebre@inria.fr
+ * @contributor jsimao@cc.isel.ipl.pt
+ */
+
 package simulation;
 
 import configuration.SimulatorProperties;
@@ -22,11 +36,24 @@ import java.util.Random;
  */
 public class SimulatorManager {
 
+    /**
+     * The list of all XVMs
+     * @see configuration.XVM
+     */
     private static LinkedList<XVM> sgVMs = null;
+    /**
+     * The list of all XHosts
+     * @see configuration.XHost
+     */
     private static LinkedList<XHost> sgHosts= null;
-
+    /**
+     * Just a stupid boolean to stop the simulation when the injector is finishing to consume events from its queue
+     */
     private static boolean endOfInjection = false;
 
+    /**
+     * When the injection is complete, we turn the endOfInjection boolean to true and kill the running daemon inside each VM
+     */
 	public static void setEndOfInjection(){
 		endOfInjection=true;
 
@@ -37,38 +64,70 @@ public class SimulatorManager {
         }
 	}
 
+    /**
+     * @return whether the injection is completed or not
+     */
 	public static boolean isEndOfInjection(){
 		return endOfInjection;
 	}
 
 
+    /**
+     * @return the collection of XVMs (i.e. the VMs running on the different hosts)
+     */
     public static Collection<XVM> getSGVMs(){
         return sgVMs;
     }
 
+    /**
+     * @return the collection of XHosts (i.e. the Hosts composing the infrastructures and on wich VMs are running)
+     * Please note that the service node (i.e. the host on wich the injector is running) is not included in the return collection.
+     * The service node is not extended as an XHost. If you need to retrieve the MSG Host instance corresponding to the service node,
+     * you should invoke the msg Host.getByName(SimulatorManager.getServiceNodeName()).
+     */
     public static Collection<XHost> getSGHosts(){
         return sgHosts;
+
     }
 
+    /**
+     * @return the name of the service node (generally node0, if you do not change the first part of the main regarding the generation
+     * of the deployment file).
+     * If you change it, please note that you should then update the getServiceNodeName code.
+     */
     public static String getServiceNodeName() {
         return "node0";
     }
 
+    /**
+     * For each MSG host (but the service node), the function creates an associated XHost.
+     * As a reminder, the XHost class extends the Host one by aggregation.
+     * At the end, all created hosts have been inserted into the sgHosts collection (see getSGHosts function)
+     * @param nbOfHosts the number of hosts that has been created by SimGrid (e.g. 10 = 1 service node + 9 hosting nodes)
+     */
     public static void initHosts(int nbOfHosts){
-     /* Since SG does not make any distinction between Host and Virtual Host (VMs and Hosts belong to the Host SG table)
-        we should retrieve first the real host in a separeted table */
-       // Please remind that node0 does not host VMs (it is a service node) and hence, it is managed separately (getServiceNodeName())
+        // Since SG does not make any distinction between Host and Virtual Host (VMs and Hosts belong to the Host SG table)
+        // we should retrieve first the real host in a separated table
+        // Please remind that node0 does not host VMs (it is a service node) and hence, it is managed separately (getServiceNodeName())
         sgHosts = new LinkedList<XHost>();
         for(int i = 1 ; i <= nbOfHosts  ; i ++){
             try {
                 Host tmp = Host.getByName("node" + i);
+                // The SimulatorProperties.getCPUCapacity returns the value indicated by nodes.cpucapacity in the simulator.properties file
                 sgHosts.add(new XHost (tmp, SimulatorProperties.getMemoryTotal(), SimulatorProperties.getNbOfCPUs(), SimulatorProperties.getCPUCapacity(), SimulatorProperties.getNetCapacity(), "127.0.0.1"));
             } catch (HostNotFoundException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                e.printStackTrace();
             }
         }
     }
 
+    /**
+     * Create and assign the VMs on the different hosts.
+     * For the moment, the initial placement follows a simple round robin strategy
+     *  The algorithm fill the first host with the n first VMs until it reaches either a CPU or a memory limit, then it switches to the second host and so on.
+     * @param nbOfHosts
+     * @param nbOfVMs
+     */
     public static void instanciateVMs(int nbOfHosts, int nbOfVMs) {
 
         int nodeIndex = 0;
@@ -90,7 +149,7 @@ public class SimulatorManager {
             nodeMemCons[nodeIndex] = 0;
 
             //The initialPlacement algorithm is stupid yet. It simply divides nb of VMs/nb of PMs
-            for ( i=0 ; i < nbOfVMs/nbOfHosts ; i++, vmIndex++ ){
+        /*    for ( i=0 ; i < nbOfVMs/nbOfHosts ; i++, vmIndex++ ){
                 vmClass = VMClasses.CLASSES.get(r.nextInt(nbOfVMClasses));  vmClass = VMClasses.CLASSES.get(r.nextInt(nbOfVMClasses));
                 if(nodeMemCons[nodeIndex] + vmClass.getMemSize() <= sgHostTmp.getMemSize()) {
 
@@ -113,7 +172,30 @@ public class SimulatorManager {
                     System.err.println("There is not enough memory on the physical host "+sgHostTmp.getName());
                     System.exit(1);
                 }
-            }
+            }*/
+
+            do{
+                    // Select the class for the VM
+                    vmClass = VMClasses.CLASSES.get(r.nextInt(nbOfVMClasses));  vmClass = VMClasses.CLASSES.get(r.nextInt(nbOfVMClasses));
+
+                    // Creation of the VM
+                    sgVMTmp = new XVM(sgHostTmp,"vm-" + vmIndex,
+                           vmClass.getNbOfCPUs(), vmClass.getMemSize(), vmClass.getNetBW(), null, -1, vmClass.getMigNetBW(), vmClass.getMemIntensity());
+                    sgVMs.add(sgVMTmp);
+
+                    Msg.info(String.format("vm: %s, %d, %d, %s",
+                                sgVMTmp.getName(),
+                                vmClass.getMemSize(),
+                                vmClass.getNbOfCPUs(),
+                                "NO IPs defined"
+                        ));
+                    Msg.info("vm " + sgVMTmp.getName() + " is " + vmClass.getName() + ", dp is " + vmClass.getMemIntensity());
+                    sgHostTmp.start(sgVMTmp);     // When the VM starts, its getCPUDemand equals 0
+                    nodeMemCons[nodeIndex] += sgVMTmp.getMemSize();
+
+
+             }while(nodeMemCons[nodeIndex]+vmClass.getMemSize() <= sgHostTmp.getMemSize())
+
             nodeIndex++;
         }
     }
