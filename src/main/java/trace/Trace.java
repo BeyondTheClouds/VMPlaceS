62be3d14484e723a2d599f6e16c198fbe55fc498
Jonathan <jonathan.pastor@me.com>
2014-09-17 18:03:43 +0200

    working on Trace; add singleton; first draft for hostSetState; (7)

---
+++ src/main/java/trace/Trace.java
@@ -0,0 +1,107 @@
+package trace;
+
+/**
+ * Created by jonathan on 17/09/14.
+ */
+public class Trace {
+
+    /**
+     * Declare a user state that will be associated to a given host.
+     */
+    static void hostStateDeclare(String host, String state) {
+        TraceImpl.getInstance().hostStateDeclare(host, state);
+    }
+
+    /**
+     * Declare a user state that will be associated to hosts.
+     */
+    static void hostStateDeclare(String state) {
+        TraceImpl.getInstance().hostStateDeclare(state);
+    }
+
+    /**
+     * Declare a new value for a user state associated to hosts.
+     *
+     * @param state
+     * @param value
+     * @param color
+     */
+    static void hostStateDeclareValue(String state, String value, String color) {
+        TraceImpl.getInstance().hostStateDeclareValue(state, value, color);
+    }
+
+    /**
+     * Set the user state to the given value.
+     *
+     * @param host
+     * @param state
+     * @param value
+     */
+    static void hostSetState(String host, String state, String value) {
+        TraceImpl.getInstance().hostSetState(host, state, value);
+    }
+
+    /**
+     * Pop the last value of a state of a given host.
+     *
+     * @param host
+     * @param state
+     */
+    static void hostPopState(String host, String state) {
+        TraceImpl.getInstance().hostPopState(host, state);
+    }
+
+    /**
+     * Push a new value for a state of a given host.
+     *
+     * @param host
+     * @param state
+     * @param value
+     */
+    static void hostPushState(String host, String state, String value) {
+        TraceImpl.getInstance().hostPushState(host, state, value);
+    }
+
+
+    /**
+     * Declare a new user variable associated to hosts.
+     *
+     * @param variable
+     */
+    static void hostVariableDeclare(String variable) {
+        TraceImpl.getInstance().hostVariableDeclare(variable);
+    }
+
+    /**
+     * Set the value of a variable of a host.
+     *
+     * @param host
+     * @param variable
+     * @param value
+     */
+    static void hostVariableSet(String host, String variable, double value) {
+        TraceImpl.getInstance().hostVariableSet(host, variable, value);
+    }
+
+    /**
+     * Subtract a value from a variable of a host.
+     *
+     * @param host
+     * @param variable
+     * @param value
+     */
+    static void hostVariableSub(String host, String variable, double value) {
+        TraceImpl.getInstance().hostVariableSub(host, variable, value);
+    }
+
+    /**
+     * Add a value to a variable of a host.
+     *
+     * @param host
+     * @param variable
+     * @param value
+     */
+    static void hostVariableAdd(String host, String variable, double value) {
+        TraceImpl.getInstance().hostVariableAdd(host, variable, value);
+    }
+}
