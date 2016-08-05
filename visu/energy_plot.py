65b43573a4a67cdb29cba844d458e35977d0058b
Anthony SIMONET <anthony.simonet@inria.fr>
2016-08-05 17:53:42 +0200

    plot_energy.py: deal with varying number of nodes

--- visu/energy_plot.py
+++ visu/energy_plot.py
@@ -84,11 +84,11 @@ def node_off(name, time, alg):
 # Get the number of turned off hosts
 # and of migrations
 ########################################
-n_turn_off = {True: {}, False: {}}
-n_migrations = {True: {}, False: {}}
+n_turn_off = {}
+n_migrations = {}
 algos = []
-n_off = {}
-scheduler_ticks = {True: {}, False: {}}
+n_on = {}
+scheduler_ticks = {}
 
 # load and standard deviation must be the same
 # for all the experiments in the log file
@@ -116,18 +116,31 @@ with open(sys.argv[1], 'r') as f:
         m = re.search(start_pattern, line)
         if m:
             turn_off = to_bool(m.group(6))
+            n_hosts = int(m.group(4))
+            
+            if n_hosts not in n_turn_off:
+                n_turn_off[n_hosts] = {True: {}, False: {}}
+               
+            if n_hosts not in n_migrations:
+                n_migrations[n_hosts] = {True: {}, False: {}}
+               
+            if n_hosts not in scheduler_ticks:
+                scheduler_ticks[n_hosts] = {True: {}, False: {}}
+               
+            if n_hosts not in n_on:
+                n_on[n_hosts] = {}
 
             algo = correct_name(m.group(3).split('.')[-1])
             if algo not in algos:
                 algos.append(algo)
-                scheduler_ticks[turn_off][algo] = []
+                scheduler_ticks[n_hosts][turn_off][algo] = []
 
             if turn_off:
-                n_off[algo] = {}
-                n_off[algo][0.0] = 0
+                n_on[n_hosts][algo] = {}
+                n_on[n_hosts][algo][0.0] = 0
 
-            n_turn_off[turn_off][algo] = 0
-            n_migrations[turn_off][algo] = 0
+            n_turn_off[n_hosts][turn_off][algo] = 0
+            n_migrations[n_hosts][turn_off][algo] = 0
 
             curr = turn_off
 
@@ -136,7 +149,6 @@ with open(sys.argv[1], 'r') as f:
 
             new_experiment(algo)
 
-            n_hosts = int(m.group(4))
 
             continue
 
@@ -151,18 +163,18 @@ with open(sys.argv[1], 'r') as f:
         # The scheduler is running
         m = re.search(scheduler_pattern, line)
         if m:
-            if algo not in scheduler_ticks[turn_off]:
-                scheduler_ticks[turn_off][algo] = []
-            scheduler_ticks[turn_off][algo].append(float(m.group(2)))
+            if algo not in scheduler_ticks[n_hosts][turn_off]:
+                scheduler_ticks[n_hosts][turn_off][algo] = []
+            scheduler_ticks[n_hosts][turn_off][algo].append(float(m.group(2)))
             continue
 
         # A node has been turned off
         m = re.search(off_pattern, line)
         if m:
-            n_turn_off[curr][algo] += 1
+            n_turn_off[n_hosts][curr][algo] += 1
 
             if turn_off:
-                n_off[algo][float(m.group(2))] = n_off[algo][n_off[algo].keys()[-1]] - 1
+                n_on[n_hosts][algo][float(m.group(2))] = n_on[n_hosts][algo][n_on[n_hosts][algo].keys()[-1]] - 1
 
             node_off(m.group(3), float(m.group(2)), algo)
             continue
@@ -171,7 +183,7 @@ with open(sys.argv[1], 'r') as f:
         m = re.search(on_pattern, line)
         if m:
             if turn_off:
-                n_off[algo][float(m.group(2))] = n_off[algo][n_off[algo].keys()[-1]] + 1
+                n_on[n_hosts][algo][float(m.group(2))] = n_on[n_hosts][algo][n_on[n_hosts][algo].keys()[-1]] + 1
 
             node_on(m.group(3), float(m.group(2)), algo)
             continue
@@ -179,279 +191,247 @@ with open(sys.argv[1], 'r') as f:
         # A VM has been migrated
         m = re.search(migration_pattern, line)
         if m:
-            n_migrations[curr][algo] += 1
+            n_migrations[n_hosts][curr][algo] += 1
 
 ########################################
 # Count the number of on VMs
 ########################################
-n_vms = {
-    True: {},
-    False: {}
-}
-
-violation_time = {
-    True: {},
-    False: {}
-}
-
-n_violations = {
-    True: {},
-    False: {}
-}
+n_vms = {}
 
 dir_pattern = re.compile(r'(\w+)-([\w\d]+)-(\d+)-(true|false)')
 events = os.path.join('visu', 'events')
 
 # list dir in 'visu/events'
 for item in os.listdir(events):
-        m = re.search(dir_pattern, item)
+    m = re.search(dir_pattern, item)
 
-        if m is None:
-            continue
+    if m is None:
+        continue
 
-        # look for dirs like 'centralized-algo-64'
-        if m.group(1) == 'centralized' and int(m.group(3)) == n_hosts:
-            algo = correct_name(m.group(2))
-            turn_off = to_bool(m.group(4))
+    # look for dirs like 'centralized-algo-64'
+    if m.group(1) == 'centralized':
+        algo = correct_name(m.group(2))
+        turn_off = to_bool(m.group(4))
+        n_hosts = int(m.group(3))
+        
+        if n_hosts not in n_vms:
+            n_vms[n_hosts] = { True: {}, False: {} }
 
-            event_file = os.path.join(events, item, 'events.json')
-            print('Reading ' + event_file)
+        event_file = os.path.join(events, item, 'events.json')
+        print('Reading ' + event_file)
 
-            with open(event_file, 'r') as f:
-                n_vms[turn_off][algo] = {}
-                violation_time[turn_off][algo] = 0
-                n_violations[turn_off][algo] = 0
+        with open(event_file, 'r') as f:
+            n_vms[n_hosts][turn_off][algo] = {}
 
-                last_violation = {}
+            # each line in this file is a JSON document
+            for line in f:
+                try:
+                    event = json.loads(line)
 
-                # each line in this file is a JSON document
-                for line in f:
-                    try:
-                        event = json.loads(line)
+                    if event['value'] == "NB_VM":
+                        time = float(event['time'])
+                        value = int(event['data']['value'])
+                        n_vms[n_hosts][turn_off][algo][time] = value
+                except:
+                    t, value, tb = sys.exc_info()
+                    print(str(t) + " " + str(value))
+                    print(line)
+                    traceback.print_tb(tb)
+                    sys.exit(1)
 
-                        if event['value'] == "NB_VM":
-                            time = float(event['time'])
-                            value = int(event['data']['value'])
-                            n_vms[turn_off][algo][time] = value
+                if event['value'] != 'NB_VNS_ON':
+                    continue
 
-                        if event['value'] == "NB_VM_TRUE":
-                            time = float(event['time'])
-                            value = int(event['data']['value'])
-
-                        if event['value'] == "violation-out":
-                            last_violation[event['origin']] = float(event['time'])
-                            n_violations[turn_off][algo] += 1
-
-                        if event['value'] == "normal":
-                            if event['origin'] in last_violation:
-                                violation_time[turn_off][algo] += float(event['time']) - last_violation[event['origin']]
-                            
-                                del last_violation[event['origin']]
-                                
-                    except:
-                        t, value, tb = sys.exc_info()
-                        print(str(t) + " " + str(value))
-                        print(line)
-                        traceback.print_tb(tb)
-                        #sys.exit(1)
-
-                    if event['value'] != 'NB_VNS_ON':
-                        continue
-
-                    n_vms[turn_off][algo][float(event['time'])] = int(event['data']['value'])
+                n_vms[n_hosts][turn_off][algo][float(event['time'])] = int(event['data']['value'])
 
 migration_ordered = []
 ########################################
 # Get the energy metrics
 ########################################
-energy = {True: {}, False: {}}
+energy = {}
 
 with open(sys.argv[2], 'r') as f:
-    p = re.compile(r'\d+ \w+ (\w+) (\w+) ([\d\.]+)')
+    p = re.compile(r'(\d+) \w+ (\w+) (\w+) ([\d\.]+)')
     for line in f:
         m = re.match(p, line)
-        implem = correct_name(m.group(1))
-        turn_off = to_bool(m.group(2))
-        joules = float(m.group(3))
+        n_hosts = int(m.group(1))
+        implem = correct_name(m.group(2))
+        turn_off = to_bool(m.group(3))
+        joules = float(m.group(4))
+        
+        if n_hosts not in energy:
+            energy[n_hosts] = { True: {}, False: {} }
 
-        energy[turn_off][implem] = joules / simulation_time / 1000
+        energy[n_hosts][turn_off][implem] = joules / simulation_time / 1000
 
 ########################################
 # Make the bar plot
 ########################################
-ind = np.arange(len(ORDER)) # the x locations for the groups
+ind = np.arange(len(algos)) # the x locations for the groups
 width = 0.35
 
-ordered_energy = {True: [], False: []}
-for alg in ORDER:
-    ordered_energy[True].append(energy[True][alg])
-    ordered_energy[False].append(energy[False][alg])
-    print("Inserting True %s %.2f" % (alg, energy[True][alg]))
-    print("Inserting False %s %.2f" % (alg, energy[False][alg]))
+ordered_energy = {}
+off_ordered = {}
+migration_ordered = {}
 
-print("erergy:")
-pp(energy)
-print("ordered_energy:")
-pp(ordered_energy)
-
-fig, ax1 = plt.subplots()
-
-color1 = '#888888'
-color2 = '#FFFFFF'
-linewidth = 1
-rects1 = ax1.bar(ind, ordered_energy[False], width, color=color1, linewidth=linewidth)
-rects2 = ax1.bar(ind + width, ordered_energy[True], width, color=color2, linewidth=linewidth)
-
-ax1.set_ylabel('Energy (Megawatts)')
-ax1.set_xticks(ind + width)
-lim = ax1.get_ylim()
-ax1.set_ylim(lim[0], lim[1])
-
-ax1.set_xticklabels(ORDER)
-
-########################################
-# Make the line plots
-########################################
-
-# Make sure the values here are in the same order as the energy values
-off_ordered = []
-migation_ordered = []
-for alg in ORDER:
-    off_ordered.append(n_turn_off[True][alg])
-    migration_ordered.append(n_migrations[True][alg])
-
-print("off_ordered:")
-pp(off_ordered)
-
-ax2 = ax1.twinx()
-#off_plot, = ax2.plot(ind + width, off_ordered, 'k-o', linewidth=linewidth)
-migration_plot, = ax2.plot(ind + width, migration_ordered, 'k--^', linewidth=linewidth)
-
-lim = ax2.get_ylim()
-ax2.set_ylim(lim[0], lim[1])
-ax2.set_yticks(range(0, int(math.ceil(max(migration_ordered))), 500))
-
-#for i,j in zip(ind + width, off_ordered):
-#   ax2.annotate(str(j), xy=(i,j + .5), va='bottom', weight='bold', size='large')
-
-for i,j in zip(ind + width, migration_ordered):
-    ax2.annotate(str(j), xy=(i,j + .5), va='bottom', weight='bold', size='large')
-
-lgd = ax1.legend((rects1[0], rects2[0], migration_plot),
-        ('Not turning off hosts', 'Turning off hosts', 'No. VM migrations'),
-        loc='lower right')
-
-def find_filename(format):
-    i = 0
-    path = format % i
-    while os.path.isfile(path):
-        i += 1
-        path = format % i
+for n_hosts in energy.keys():
+    if n_hosts not in ordered_energy:
+        ordered_energy[n_hosts] = { True: [], False: [] }
+        
+    for alg in ORDER:
+        if alg not in energy[n_hosts][True]:
+            continue
     
-    return path
+        ordered_energy[n_hosts][True].append(energy[n_hosts][True][alg])
+        ordered_energy[n_hosts][False].append(energy[n_hosts][False][alg])
 
-save_path = find_filename('energy_%d_%d_%%d.pdf' % (load, std))
-plt.savefig(save_path, transparent=True, bbox_extra_artists=(lgd,), bbox_inches='tight')
-print('Saved plot as ' + save_path)
-if os.system('which imgcat > /dev/null 2>&1') == 0:
-    os.system('imgcat ' + save_path)
+    print("ordered_energy %d:" % n_hosts)
+    pp(ordered_energy[n_hosts])
+
+    fig, ax1 = plt.subplots()
+
+    color1 = '#888888'
+    color2 = '#FFFFFF'
+    linewidth = 1
+    rects1 = ax1.bar(ind, ordered_energy[n_hosts][False], width, color=color1, linewidth=linewidth)
+    rects2 = ax1.bar(ind + width, ordered_energy[n_hosts][True], width, color=color2, linewidth=linewidth)
+
+    ax1.set_ylabel('Energy (Megawatts)')
+    ax1.set_xticks(ind + width)
+    lim = ax1.get_ylim()
+    ax1.set_ylim(lim[0], lim[1])
+
+    ax1.set_xticklabels(ORDER)
+
+    ########################################
+    # Make the line plots
+    ########################################
+    # Make sure the values here are in the same order as the energy values
+    off_ordered[n_hosts] = []
+    migration_ordered[n_hosts] = []
+    for alg in ORDER:
+        if alg not in n_turn_off[n_hosts][True]:
+            continue
+        
+        off_ordered[n_hosts].append(n_turn_off[n_hosts][True][alg])
+        migration_ordered[n_hosts].append(n_migrations[n_hosts][True][alg])
+
+    print("off_ordered[%d]:" % n_hosts)
+    pp(off_ordered[n_hosts])
+    
+    print("migration_ordered[%d]:" % n_hosts)
+    print(migration_ordered[n_hosts])
+
+    ax2 = ax1.twinx()
+    migration_plot, = ax2.plot(ind + width, migration_ordered[n_hosts], 'k--^', linewidth=linewidth)
+
+    lim = ax2.get_ylim()
+    ax2.set_ylim(lim[0], lim[1])
+    ax2.set_yticks(range(0, int(math.ceil(max(migration_ordered[n_hosts]))), 500))
+
+    for i,j in zip(ind + width, migration_ordered[n_hosts]):
+        ax2.annotate(str(j), xy=(i,j + .5), va='bottom', weight='bold', size='large')
+
+    lgd = ax1.legend((rects1[0], rects2[0], migration_plot),
+            ('Not turning off hosts', 'Turning off hosts', 'No. VM migrations'),
+            loc='lower right')
+
+    def find_filename(format):
+        i = 0
+        path = format % i
+        while os.path.isfile(path):
+            i += 1
+            path = format % i
+        
+        return path
+
+    save_path = find_filename('energy_%d_%d_%d_%%d.pdf' % (n_hosts, load, std))
+    plt.savefig(save_path, transparent=True, bbox_extra_artists=(lgd,), bbox_inches='tight')
+    print('Saved plot as ' + save_path)
+    if os.system('which imgcat > /dev/null 2>&1') == 0:
+        os.system('imgcat ' + save_path)
 
 
 ########################################
 # Make n_on plot
 ########################################
-fig, ax1 = plt.subplots()
-
 ordered_n_on = {}
 plots = {}
+
 styles = ['k-o', 'k-^', 'k-v', 'k-*']
-i = 0
-for alg in ORDER:
-    ordered_n_on[alg] = sorted(n_off[alg].items())
-    plots[alg], = ax1.plot(map(lambda t: t[0], ordered_n_on[alg]),
-            map(lambda t: t[1], ordered_n_on[alg]), styles[i], linewidth=linewidth, ms=8)
-    i += 1
 
-lgd = ax1.legend(plots.values(),
-        n_off.keys(),
-        loc='upper right')
+for n_hosts in n_on:
+    fig, ax1 = plt.subplots()
 
-ax1.set_xlim(0, simulation_time)
-ax1.set_ylim(20, n_hosts)
+    if n_hosts not in ordered_n_on:
+        ordered_n_on[n_hosts] = {}
 
-save_path = find_filename('n_on_%d_%d_%%d.pdf' % (load, std))
-plt.savefig(save_path, transparent=True, bbox_extra_artists=(lgd,), bbox_inches='tight')
-print('Saved plot as ' + save_path)
-if os.system('which imgcat > /dev/null 2>&1') == 0:
-    os.system('imgcat ' + save_path)
+    if n_hosts not in plots:
+        plots[n_hosts] = {}
+
+    i = 0
+    for alg in ORDER:
+        if alg not in n_on[n_hosts]:
+            continue
+        
+        ordered_n_on[n_hosts][alg] = sorted(n_on[n_hosts][alg].items())
+        plots[n_hosts][alg], = ax1.plot(map(lambda t: t[0], ordered_n_on[n_hosts][alg]),
+                map(lambda t: t[1], ordered_n_on[n_hosts][alg]), styles[i], linewidth=linewidth, ms=8)
+        i += 1
+     
+    print("ordered_n_on[%d]" % n_hosts)
+    pp(ordered_n_on[n_hosts])
+
+    lgd = ax1.legend(plots[n_hosts].values(),
+            n_on[n_hosts].keys(),
+            loc='upper right')
+
+    ax1.set_xlim(0, simulation_time)
+    ax1.set_ylim(20, n_hosts)
+
+    save_path = find_filename('n_on_%d_%d_%d_%%d.pdf' % (n_hosts, load, std))
+    plt.savefig(save_path, transparent=True, bbox_extra_artists=(lgd,), bbox_inches='tight')
+    print('Saved plot as ' + save_path)
+    if os.system('which imgcat > /dev/null 2>&1') == 0:
+        os.system('imgcat ' + save_path)
 
 ########################################
 # Make vm_on plot
 ########################################
 n_vms_ordered = {}
 
-fig, ax1 = plt.subplots()
-
 linewidth = 1
 
-print("n_vms:")
-pp(n_vms[True].keys())
+for n_hosts in n_vms:
+    fig, ax1 = plt.subplots()
 
-i = 0
-colors = ['g', 'b', 'm', 'y']
-for alg in ORDER:
-    n_vms_ordered[alg] = sorted(n_vms[True][alg].items())
-    plots[alg], = ax1.plot(map(lambda t: t[0], n_vms_ordered[alg]),
-            map(lambda t: t[1], n_vms_ordered[alg]), colors[i] + '.-', linewidth=linewidth, ms=8)
-    
-    #for tick in scheduler_ticks[True][alg]:
-    #   ax1.plot((tick, tick), (450, 512), colors[i] + '-')
+    if n_hosts not in n_vms_ordered:
+        n_vms_ordered[n_hosts] = {}
 
-    i += 1
+    i = 0
+    colors = ['g', 'b', 'm', 'y']
+    for alg in ORDER:
+        if alg not in n_vms[n_hosts][True]:
+            continue
+        
+        n_vms_ordered[n_hosts][alg] = sorted(n_vms[n_hosts][True][alg].items())
+        plots[n_hosts][alg], = ax1.plot(map(lambda t: t[0], n_vms_ordered[n_hosts][alg]),
+                map(lambda t: t[1], n_vms_ordered[n_hosts][alg]), colors[i] + '.-', linewidth=linewidth, ms=8)
+        
+        #for tick in scheduler_ticks[n_hosts][True][alg]:
+        #   ax1.plot((tick, tick), (450, 512), colors[i] + '-')
 
-ax1.set_xlim(0, simulation_time)
+        i += 1
 
-lgd = ax1.legend(plots.values(),
-        n_off.keys(),
-        loc='lower right')
+    ax1.set_xlim(0, simulation_time)
 
-save_path = find_filename('vms_on_%d_%d_%%d.pdf' % (load, std))
-plt.savefig(save_path, transparent=True, bbox_extra_artists=(lgd,), bbox_inches='tight')
-print('Saved plot as ' + save_path)
-if os.system('which imgcat > /dev/null 2>&1') == 0:
-    os.system('imgcat ' + save_path)
+    lgd = ax1.legend(plots[n_hosts].values(),
+            n_on[n_hosts].keys(),
+            loc='lower right')
 
-sys.exit(1)
+    save_path = find_filename('vms_on_%d_%d_%d_%%d.pdf' % (n_hosts, load, std))
+    plt.savefig(save_path, transparent=True, bbox_extra_artists=(lgd,), bbox_inches='tight')
+    print('Saved plot as ' + save_path)
+    if os.system('which imgcat > /dev/null 2>&1') == 0:
+        os.system('imgcat ' + save_path)
 
-########################################
-# Make the violations plot
-########################################
-ind = np.arange(len(ORDER)) # the x locations for the groups
-width = 0.35
-
-ordered_violation_time = {True: [], False: []}
-ordered_n_violations = {True: [], False: []}
-for alg in ORDER:
-    ordered_violation_time[False].append(energy[False][alg])
-    ordered_n_violations[True].append(energy[True][alg])
-
-fig, ax1 = plt.subplots()
-
-color1 = '#888888'
-color2 = '#FFFFFF'
-linewidth = 1
-rects1 = ax1.bar(ind, ordered_violation_time[False], width, color=color1, linewidth=linewidth)
-rects2 = ax1.bar(ind + width, ordered_violation_time[True], width, color=color2, linewidth=linewidth)
-
-ax1.set_ylabel('Violation time (seconds)')
-ax1.set_xticks(ind + width)
-lim = ax1.get_ylim()
-ax1.set_ylim(lim[0], lim[1])
-
-ax1.set_xticklabels(ORDER)
-
-save_path = find_filename('violations_%d_%d_%%d.pdf' % (load, std))
-plt.savefig(save_path, transparent=True, bbox_extra_artists=(lgd,), bbox_inches='tight')
-print('Saved plot as ' + save_path)
-if os.system('which imgcat > /dev/null 2>&1') == 0:
-    os.system('imgcat ' + save_path)
